57f64c8d5202484737a3f1b1e21f3db1f78de47d
beefbarn <70672339+beefbarn@users.noreply.github.com>
2024-06-17 01:16:16 -0400

    Version 06.17.24

---
+++ first-site/scripts/main.js
@@ -0,0 +1,35 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/freaky-guy.jpg") {
+    myImage.setAttribute("src", "images/dealer-1.png");
+  } else {
+    myImage.setAttribute("src", "images/freaky-guy.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `You're fucked, ${myName}`;
+    }
+}
+
+//Initialization code block below:
+if (!localStorage.getItem("name")) {
+    setUserName();
+    } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `You're fucked, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+};
